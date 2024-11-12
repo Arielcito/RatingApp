16c127bdda78a7a0417351bb83f00445eec5e009
Diego Wald <diego.wald@gmail.com>
2024-11-12 11:02:46 -0300

    duplicamos clientes para lograr efecto de movimiento

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -31,7 +31,37 @@ export const clientsData: Client[] = [
     link: "#",
     width: 106,
     height: 32,
-  }/*,
+  },
+  {
+    logo: "https://drive.google.com/uc?id=1RLVPyza7W8zCnI65qYy77Z0ew8uJV__Z&export=download",
+    link: "#",
+    width: 99,
+    height: 24,
+  },
+  {
+    logo: "https://drive.google.com/uc?id=1qLBJQi10lX_dw5GHAvJK9OnTtau2Z-RY&export=download",
+    link: "#",
+    width: 89,
+    height: 32,
+  },
+  {
+    logo: "https://drive.google.com/uc?id=1gTI0zTHbAkAfe6Mw6VFxyHpIeFnfGABb&export=download",
+    link: "#",
+    width: 120,
+    height: 24,
+  },
+  {
+    logo: "https://drive.google.com/uc?id=1vb9vlfDQTcWKRp2kqeKUpM5VrSL89Sy6&export=download",
+    link: "#",
+    width: 108,
+    height: 32,
+  },
+  {
+    logo: "https://drive.google.com/uc?id=1l5KmcpWB-jTNbetKY0LoEIyEgOP7Mj11&export=download",
+    link: "#",
+    width: 106,
+    height: 32,
+  }  /*,
   {
     logo: "/images/logo/logo.png",
     link: "#",
