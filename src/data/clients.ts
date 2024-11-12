f9070a2e9078149d2750bf19ce626a79c5dd8144
Diego Wald <diego.wald@gmail.com>
2024-11-11 22:14:03 -0300

    pruebas

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -21,6 +21,12 @@ export const clientsData: Client[] = [
     height: 24,
   },
   {
+    logo: "https://drive.google.com/uc?id=1gTI0zTHbAkAfe6Mw6VFxyHpIeFnfGABb&export=download",
+    link: "#",
+    width: 120,
+    height: 24,
+  },
+  {
     logo: "https://drive.google.com/uc?id=1vb9vlfDQTcWKRp2kqeKUpM5VrSL89Sy6&export=download",
     link: "#",
     width: 108,
@@ -31,7 +37,7 @@ export const clientsData: Client[] = [
     link: "#",
     width: 106,
     height: 32,
-  },
+  }/*,
   {
     logo: "/images/logo/logo.png",
     link: "#",
@@ -43,5 +49,5 @@ export const clientsData: Client[] = [
     link: "#",
     width: 106,
     height: 32,
-  }
+  }*/
 ]; 
