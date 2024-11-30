d213a05725686860a57775947aa00402485fc1d3
Ariel Serato <Serato.arieli@gmail.com>
2024-11-30 18:07:44 -0300

    feat: add video support

--- src/data/news.ts
+++ src/data/news.ts
@@ -5,43 +5,38 @@ export interface News {
   date: string;
   image: string;
   category: string;
+  videoUrl?: string;
+  type: 'image' | 'video';
 }
 
 export const castingNews: News[] = [
-  /*{
-      id: "1",
-      title: "Gran convocatoria para nuevo reality show",
-      description: "Más de 1000 participantes se presentaron en el primer día de audiciones para el nuevo programa de talentos.",
-      date: "2024-03-15",
-      image: "/images/news/casting-1.jpg",
-      category: "Convocatorias"
-    },*/
   {
     id: "1",
     title: "CEPBA y BBVA. Herramientas para potenciar tus finanzas. Sin costo",
-    description:
-      "Herramientas para potenciar tus finanzas. Descubri nuevas herramientas para hacer rendir tu capital. Exponen: Manuela Moralejo e Ivan Fernandez",
+    description: "Herramientas para potenciar tus finanzas. Descubri nuevas herramientas para hacer rendir tu capital. Exponen: Manuela Moralejo e Ivan Fernandez",
     date: "2024-11-25 16:00",
-    image:
-      "https://drive.google.com/uc?id=1gpzL0a4YezL2DMgGdA7ObE07T_vY0We_&export=download",
+    image: "https://drive.google.com/uc?id=1gpzL0a4YezL2DMgGdA7ObE07T_vY0We_&export=download",
     category: "Actividad Virtual",
+    type: 'image'
   },
   {
     id: "2",
     title: "Becas para jóvenes talentos",
-    description:
-      "Se abre nueva convocatoria para becas de formación artística en las principales academias del país.",
+    description: "Se abre nueva convocatoria para becas de formación artística en las principales academias del país.",
     date: "2024-03-10",
-    image: "/images/news/casting-2.jpg",
+    image: "",
     category: "Becas",
+    videoUrl: "1WPYdsUP_VXov3gMKZgB2U1Hx8ZdLNX9a",
+    type: 'video'
   },
   {
     id: "3",
     title: "Éxito en el programa de mentorías",
-    description:
-      "Los participantes del programa de mentorías muestran resultados prometedores en sus carreras artísticas.",
+    description: "Los participantes del programa de mentorías muestran resultados prometedores en sus carreras artísticas.",
     date: "2024-03-05",
-    image: "/images/news/casting-3.jpg",
+    image: "",
     category: "Mentorías",
+    videoUrl: "1WPYdsUP_VXov3gMKZgB2U1Hx8ZdLNX9a",
+    type: 'video'
   },
 ];
