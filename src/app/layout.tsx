9d1acac8c9de8301b0126e3df61c1da3f5a50c2e
Arielcito <serato.arieli@gmail.com>
2024-12-07 19:28:13 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className="dark" suppressHydrationWarning>
-      <body className="bg-background dark:bg-blacksection overflow-hidden">
+      <body className="overflow-hidden">
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
