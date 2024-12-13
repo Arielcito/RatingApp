d63b31253ca4c5a710257405129be6c410d38333
Diego Wald <diego.wald@gmail.com>
2024-12-13 14:01:16 -0300

    repito metricas por expertos

--- src/data/avalados-por-expertos.ts
+++ src/data/avalados-por-expertos.ts
@@ -28,20 +28,22 @@ export const avaladosPorExpertos: AvaladosPorExpertos[] = [
   },
   {
     id: "2",
-    title: "Becas para jóvenes talentos",
+    title: "CEPBA y BBVA. Herramientas para potenciar tus finanzas. Sin costo",
     description:
-      "Se abre nueva convocatoria para becas de formación artística en las principales academias del país.",
-    date: "2024-03-10",
-    image: "/images/news/casting-2.jpg",
-    category: "Becas",
+      "Herramientas para potenciar tus finanzas. Descubri nuevas herramientas para hacer rendir tu capital. Exponen: Manuela Moralejo e Ivan Fernandez",
+    date: "2024-11-25 16:00",
+    image:
+      "https://drive.google.com/uc?id=1gpzL0a4YezL2DMgGdA7ObE07T_vY0We_&export=download",
+    category: "Actividad Virtual",
   },
   {
     id: "3",
-    title: "Éxito en el programa de mentorías",
+    title: "CEPBA y BBVA. Herramientas para potenciar tus finanzas. Sin costo",
     description:
-      "Los participantes del programa de mentorías muestran resultados prometedores en sus carreras artísticas.",
-    date: "2024-03-05",
-    image: "/images/news/casting-3.jpg",
-    category: "Mentorías",
+      "Herramientas para potenciar tus finanzas. Descubri nuevas herramientas para hacer rendir tu capital. Exponen: Manuela Moralejo e Ivan Fernandez",
+    date: "2024-11-25 16:00",
+    image:
+      "https://drive.google.com/uc?id=1gpzL0a4YezL2DMgGdA7ObE07T_vY0We_&export=download",
+    category: "Actividad Virtual",
   },
 ];
