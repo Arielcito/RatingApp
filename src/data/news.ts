99acd48047a2cff92d0989de677bab958f9099ea
Diego Wald <diego.wald@gmail.com>
2024-12-13 13:59:15 -0300

    se incorporan imagenes en castingApp

--- src/data/news.ts
+++ src/data/news.ts
@@ -6,37 +6,42 @@ export interface News {
   image: string;
   category: string;
   videoUrl?: string;
-  type: 'image' | 'video';
+  type: "image" | "video";
 }
 
 export const castingNews: News[] = [
   {
     id: "1",
     title: "CEPBA y BBVA. Herramientas para potenciar tus finanzas. Sin costo",
-    description: "Herramientas para potenciar tus finanzas. Descubri nuevas herramientas para hacer rendir tu capital. Exponen: Manuela Moralejo e Ivan Fernandez",
+    description:
+      "Herramientas para potenciar tus finanzas. Descubri nuevas herramientas para hacer rendir tu capital. Exponen: Manuela Moralejo e Ivan Fernandez",
     date: "2024-11-25 16:00",
-    image: "https://drive.google.com/uc?id=1gpzL0a4YezL2DMgGdA7ObE07T_vY0We_&export=download",
+    image:
+      "https://drive.google.com/uc?id=1gpzL0a4YezL2DMgGdA7ObE07T_vY0We_&export=download",
     category: "Actividad Virtual",
-    type: 'image'
+    type: "image",
   },
   {
     id: "2",
     title: "Becas para jóvenes talentos",
-    description: "Se abre nueva convocatoria para becas de formación artística en las principales academias del país.",
+    description:
+      "Se abre nueva convocatoria para becas de formación artística en las principales academias del país.",
     date: "2024-03-10",
-    image: "",
     category: "Becas",
-    videoUrl: "1WPYdsUP_VXov3gMKZgB2U1Hx8ZdLNX9a",
-    type: 'video'
+    image:
+      "https://drive.google.com/uc?id=1KxeXkygW8chTPjN0bamhJyes2J1WRDBk&export=download",
+    type: "video",
   },
   {
     id: "3",
     title: "Éxito en el programa de mentorías",
-    description: "Los participantes del programa de mentorías muestran resultados prometedores en sus carreras artísticas.",
+    description:
+      "Los participantes del programa de mentorías muestran resultados prometedores en sus carreras artísticas.",
     date: "2024-03-05",
-    image: "",
+    image:
+      "https://drive.google.com/uc?id=19t8chupuQhwhg7bcFcj_BbkqvHkNrGnA&export=download",
     category: "Mentorías",
     videoUrl: "1WPYdsUP_VXov3gMKZgB2U1Hx8ZdLNX9a",
-    type: 'video'
+    type: "video",
   },
 ];
