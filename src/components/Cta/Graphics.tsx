b5bc009e31599386be0b74866be0cdabb21330aa
Arielcito <serato.arieli@gmail.com>
2025-06-26 08:54:10 -0300

    fix links

--- src/components/Cta/Graphics.tsx
+++ src/components/Cta/Graphics.tsx
@@ -56,60 +56,7 @@ const Graphics = () => {
         </svg>
       </div>
 
-      <div className="absolute left-0 top-10 -z-10 hidden sm:block">
-        <svg
-          width="706"
-          height="1405"
-          viewBox="0 0 706 1405"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-[400px] md:w-[700px]"
-        >
-          <g opacity="0.25" filter="url(#filter0_f_47_21)">
-            <circle
-              cx="3.5"
-              cy="702.5"
-              r="442.5"
-              fill="url(#paint0_linear_47_21)"
-            />
-          </g>
-          <defs>
-            <filter
-              id="filter0_f_47_21"
-              x="-699"
-              y="0"
-              width="1405"
-              height="1405"
-              filterUnits="userSpaceOnUse"
-              colorInterpolationFilters="sRGB"
-            >
-              <feFlood floodOpacity="0" result="BackgroundImageFix" />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                stdDeviation="130"
-                result="effect1_foregroundBlur_47_21"
-              />
-            </filter>
-            <linearGradient
-              id="paint0_linear_47_21"
-              x1="-545.385"
-              y1="1145"
-              x2="552.329"
-              y2="380.732"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#8EA5FE" />
-              <stop offset="0.541667" stopColor="#BEB3FD" />
-              <stop offset="1" stopColor="#90D1FF" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
+   
     </>
   );
 };
