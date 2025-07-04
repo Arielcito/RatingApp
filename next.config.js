f9375c2766c4070208982156b07b4dc1d3f77df2
Arielcito <serato.arieli@gmail.com>
2025-07-04 15:00:59 -0300

    change setting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    loader: 'default',
-    unoptimized: false,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
